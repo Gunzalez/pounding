db17f9c8dba05fe397b50b572640179f23500462
Segun Konibire <segun.konibire@valtech.co.uk>
2016-05-31 17:05:38 +0100

    Adds scroll detection directive

--- app/app.js
+++ app/app.js
@@ -18,4 +18,19 @@ angular
             .otherwise({
                 redirectTo: '/'
             });
-    }]);
+    }])
+
+    .directive("scroll", function ($window) {
+        return function(scope, element) {
+            angular.element($window).bind("scroll", function() {
+                var topBarEl = element[0].getElementsByClassName('top-bar');
+                var topBar = angular.element(topBarEl)
+                if (this.pageYOffset > 10) {
+                    topBar.removeClass('top');
+                } else {
+                    topBar.addClass('top');
+                }
+                scope.$apply();
+            });
+        };
+    });
