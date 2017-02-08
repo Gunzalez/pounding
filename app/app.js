fdd08f60dc6799db197a7ed7041dd87e37bd25a8
Segun Konibire <gunzalez@hotmail.com>
2017-02-08 00:12:57 +0000

    Adds in progress bar

--- app/app.js
+++ app/app.js
@@ -33,4 +33,11 @@ angular
                 scope.$apply();
             });
         };
+    })
+
+    .filter('time', function() {
+        return function(seconds) {
+            var hh = Math.floor(seconds / 3600), mm = Math.floor(seconds / 60) % 60, ss = Math.floor(seconds) % 60;
+            return hh + ":" + (mm < 10 ? "0" : "") + mm + ":" + (ss < 10 ? "0" : "") + ss;
+        };
     });
--- app/controllers/detailController.js
+++ app/controllers/detailController.js
@@ -1,16 +1,19 @@
 
 angular
     .module('yamApp')
-    .controller('detailController', ['$scope', 'mealsService', '$routeParams', '$rootScope', function ($scope, mealsService, $routeParams, $rootScope) {
+    .controller('detailController', ['$scope', 'mealsService', '$routeParams', '$rootScope', '$interval', function ($scope, mealsService, $routeParams, $rootScope, $interval) {
 
         window.scroll(0,0);
 
         $scope.selectedMeal = {};
         $scope.videoDisplay = document.getElementById('videoEl');
-
         $scope.videoPlaying = false;
         $scope.currentTime = 0;
         $scope.totalTime = 0;
+        $scope.vidHeightCenter = -1000;
+        $scope.vidWidthCenter = -1000;
+        $scope.scrubTop = -1000;
+        $scope.scrubLeft = -1000;
 
         $scope.initPlayer = function(){
             $scope.currentTime = 0;
@@ -20,14 +23,34 @@ angular
         };
 
         $scope.updateLayout = function(){
+            $scope.scrubTop = document.getElementById('progressMeterFull').offsetTop-2;
+            $scope.vidHeightCenter = $scope.videoDisplay.offsetHeight/2 -50;
+            $scope.vidWidthCenter = $scope.videoDisplay.offsetWidth/2 -50;
             if(!$scope.$$phase){
                 $scope.$apply();
             }
         };
 
+        $interval(function(){
+            var t = $scope.videoDisplay.currentTime;
+            var d = $scope.videoDisplay.duration;
+            //var w = t / d * 100;
+            var p = document.getElementById('progressMeterFull').offsetLeft + document.getElementById('progressMeterFull').offsetWidth;
+            $scope.scrubLeft = (t / d * p) - 7;
+            $scope.updateLayout();
+        }, 100);
+
         $scope.updateTime = function(e){
             $scope.currentTime = e.target.currentTime;
-            $scope.updateLayout()
+            var playBtn = document.getElementById('playBtn'),
+                    $playBtn = angular.element(playBtn);
+            if($scope.currentTime == $scope.totalTime){
+                $scope.videoDisplay.pause();
+                $scope.videoPlaying = false;
+                $scope.currentTime = 0;
+                $playBtn.find('span').toggleClass("glyphicon-play", true);
+                $playBtn.find('span').toggleClass("glyphicon-pause", false);
+            }
         };
 
         $scope.updateDate = function(e){
